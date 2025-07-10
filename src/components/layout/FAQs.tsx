6ae21b5586f3640ab81699ac27a03fc456717f4b
mraihanaf <mraihanaf0@gmail.com>
2025-07-10 15:12:37 +0700

    feat: add px in faq section

--- src/components/layout/FAQs.tsx
+++ src/components/layout/FAQs.tsx
@@ -7,11 +7,11 @@ export default function FAQs() {
       <div className="my-10">
         <h2 className="text-3xl font-bold tracking-tight leading-tight text-center mx-10 sm:text-5xl">Frequently Asked Questions</h2>
       </div>
-      <div className="mx-10 sm:mx-64 md:mx-32">
-        <Accordion type="single" collapsible className="w-full">
+      <div className="mx-10 sm:mx-64 md:mx-32 sm:px-64">
+        <Accordion type="single" collapsible className="w-full ">
           {FAQsData.map((item, index) => (
             <AccordionItem value={`item-${index}`} key={`item-${index}`}>
-                <AccordionTrigger className="text-base sm:text-lg font-semibold">{item.question}</AccordionTrigger>
+                <AccordionTrigger className="text-base sm:text-xl font-semibold">{item.question}</AccordionTrigger>
                 <AccordionContent className="flex flex-col gap-4 m-1 sm:text-base">
                   {item.answer}
                 </AccordionContent>
