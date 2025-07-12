d9cf55c2578cbf22f695c41edf8723c4b1da8d54
mraihanaf <mraihanaf0@gmail.com>
2025-07-12 22:55:17 +0700

    fix: faq layout

--- src/components/layout/FAQs.tsx
+++ src/components/layout/FAQs.tsx
@@ -4,10 +4,10 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 export default function FAQs() {
   return (
     <div className="relative min-h-screen bg-gradient-to-b from-black to-gray-600/20 text-white overflow-hidden" id="faqs">
-      <div className="my-10">
+      <div className="my-10 sm:mb-20">
         <h2 className="text-3xl font-bold tracking-tight leading-tight text-center mx-10 sm:text-5xl">Frequently Asked Questions</h2>
       </div>
-      <div className="mx-10 sm:mx-64 md:mx-32 lg:px-64">
+      <div className="mx-10 sm:mx-32 md:mx-32 xl:px-64">
         <Accordion type="single" collapsible className="w-full ">
           {FAQsData.map((item, index) => (
             <AccordionItem value={`item-${index}`} key={`item-${index}`}>
